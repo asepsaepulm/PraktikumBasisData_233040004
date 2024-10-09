2aeaa79bf6461ed8c7ddf79d399c23ff0c5dabee
Asep Saepul Milah <146751939+asepsaepulmilah2003@users.noreply.github.com>
2024-10-09 12:10:59 +0700

    first commit

---
+++ SQLQuery_4.sql
@@ -0,0 +1,7 @@
+INSERT INTO mahasiswa (ID, NIM, NAMA, JURUSAN, TANGGAL_LAHIR) VALUES
+(1, '203040171', 'Budi Santoso', 'Teknik Informatika', '2000-02-15'),
+(2, '233040001', 'Asep Saepul Milah', 'Teknik Informatika', '2003-02-15'),
+(3, '203040172', 'Budi Darmono', 'Teknik Informatika', '2004-02-15'),
+(4, '203040173', 'Dono Santoso', 'Teknik Informatika', '2005-02-15'),
+(5, '203040174', 'Helmi Hendra', 'Teknik Informatika', '2006-02-15'),
+(6, '203040175', 'Soebandono', 'Teknik Informatika', '2007-02-15'); 
